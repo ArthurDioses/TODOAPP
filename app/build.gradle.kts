d04c3ce8097286685a0ffd7f1e8df5890c98cf50
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-03-18 11:21:38 -0500

    Impl() - Dependency LiveData

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     //Hilt
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
+    //LiveData
+    implementation("androidx.compose.runtime:runtime-livedata")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
